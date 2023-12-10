f047f4c4507ed9834cd4fe813114b278dfd799f4
ehjohnson154 <spiffy154@gmail.com>
2023-12-10 14:56:50 -0800

    cleaned up code

--- FinalProject/chat_server.py
+++ FinalProject/chat_server.py
@@ -1,11 +1,13 @@
-#Note: Couldn't figure out buffer system in time. My apologies!
-
 import sys
 import socket
 import select
 import json
 
 PACKET_LEN_SIZE = 2
+
+#global dicts
+name_list = {}
+buffer_list = {}
 
 def build_packet(type, words, name):
     final_packet = b''
@@ -20,15 +22,16 @@ def build_packet(type, words, name):
 
     return final_packet
 
-def send_data(stuff, socket):
-    #string_to_send = stuff
-    #string_bytes = string_to_send.encode()
+#send a encoded message to a select socket
+def send_data(stuff, socket): 
     socket.send(stuff)
 
+#send a server message to specific socket
 def send_server_message(type, words, name, socket):
     stuff = build_packet(type, words, name)
     send_data(stuff, socket)
 
+#sends a client message or server message to all clients
 def distribute_message(message, listener, sender, set):
     for s in set:
         if s != sender and s != listener:
@@ -37,12 +40,13 @@ def distribute_message(message, listener, sender, set):
             else:
                 send_server_message("message", message, name_list[sender], s)
 
-
+#decodes a encoded packet, assuming 2b stating length
 def decode_message(packet):
     cutpacket = packet[2:] #cut out the length part of packet
     decodedword = cutpacket.decode()
     return decodedword
 
+#buffer to return a encoded packet from buffer
 def get_next_message(s):
     #checking to make sure we have a valid word packet
     if len(buffer_list[s]) >= 2: 
@@ -60,14 +64,9 @@ def get_next_message(s):
         return None
 
 
-#     pass
-name_list = {}
-buffer_list = {}
 
 def run_server(port):
-
-
-
+    #read set storing sockets
     read_set = set()
 
     listen = socket.socket()
@@ -77,26 +76,26 @@ def run_server(port):
     
     read_set.add(listen)
 
-    
-    #buffer_list = {}
-
     # main loop:
     while(True):
         #     call select() and get the sockets that are ready to read
         ready_to_read, _, _ = select.select(read_set, {}, {})
 
-
+        #goes through all ready to read sockets
         for s in ready_to_read:
+            #add new socket if listener socket detects incoming connection
             if s is (listen):
                 new_connection, info = s.accept()
                 read_set.add(new_connection)
                 print(f'{info} connected...')
                 buffer_list[new_connection] = b''
 
+            #if socket, determine what type of message
             else:
                 data = s.recv(4096)
                 buffer_list[s] += data
                 encodedmessage = get_next_message(s)
+                #if no data, means client disconnected
                 if len(data) == 0:
                     disconnnectedmessage = f'***{name_list[s]} is disconnected...'
                     distribute_message(disconnnectedmessage, listen, listen, read_set)
@@ -105,16 +104,19 @@ def run_server(port):
                     del(buffer_list[s])
                     read_set.remove(s)
 
+                #if getnextmessage returns none, means theres not enough for full packet
                 if encodedmessage != None:
                     #message = decode_message(data)
                     message = decode_message(encodedmessage)
                     stuff = json.loads(message)
+                    #if type hello, means initial handshake message
                     if stuff["type"] == "hello":
                         name_list[s] = stuff["chat"]
                         connectedmessage = f'***{name_list[s]} is connected...'
                         distribute_message(connectedmessage,listen, listen, read_set)
                         print(connectedmessage)
-                        
+                    
+                    #else, normal message to distribute to other clients
                     else:
                         print(f'{name_list[s]}: {stuff["chat"]}')
                         distribute_message(stuff["chat"],listen, s, read_set)
